6943366acaec8f443e906180467dbd97ebb4b16b
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2013-07-12 12:21:10 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+castles=$HOME/.homesick/repos
+homeshick=$castles/homeshick/home/.homeshick
+git clone git://github.com/andsens/homeshick.git $castles/homeshick
+$homeshick clone git://github.com/koelnconcert/dotfiles.git
+
