f9219d4ab5ce1229fbb192babd0a97fd4b6d2cf9
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2014-09-03 15:51:26 +0200

    check for git in install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+command -v git >/dev/null 2>&1 || { echo >&2 "git it not installed.  Aborting."; exit 1; }
 castles=$HOME/.homesick/repos
 git clone git://github.com/andsens/homeshick.git $castles/homeshick
 source $HOME/.homesick/repos/homeshick/homeshick.sh
