725ffefc6bdf79667493ddcef8bcfc4e7c06f60f
Sebastian Peters <sebastian.peters@tib.eu>
2019-03-14 15:17:09 +0100

    fix/update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,4 @@ castles=$HOME/.homesick/repos
 git clone git://github.com/andsens/homeshick.git $castles/homeshick
 source $HOME/.homesick/repos/homeshick/homeshick.sh
 homeshick clone -b git://github.com/koelnconcert/dotfiles.git
-homeshick link
-
-if [[ `git version`  =~ git\ version\ 1\..* ]]; then
-  echo "Old git version found; remove push.default config"
-  git config -f ~/.gitconfig --unset push.default
-fi
-
-
-
+homeshick link -f
